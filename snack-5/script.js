a095acc3893109ce463c6a56e27bbab1bb554f4d
anthony-murè <tony.mure99@gmail.com>
2025-07-17 15:04:46 +0200

    [add]snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,14 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const numsEven = nums.filter((number) =>{
+ 
+    if(number % 2 === 0){
+
+      return true;
+    }
+
+});
+
+console.log(numsEven);

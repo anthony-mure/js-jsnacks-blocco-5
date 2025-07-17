abab55b15191682e2ead24a0beffbe16bfe50cec
anthony-murè <tony.mure99@gmail.com>
2025-07-17 15:37:42 +0200

    [add]snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,13 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student = students.find(element => element.name === 'Marco Lanci');
+
+
+const studentClass = student.class;
+
+console.log(studentClass);
+
+
+
